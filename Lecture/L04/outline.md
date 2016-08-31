949e87870027e663b451ea1f3cb5af9c9a461f51
Dylan Mikesell <dylan.mikesell@gmail.com>
2016-08-31 11:21:39 -0600

    Added lecture 4 materials.

--- Lecture/L04/outline.md
+++ Lecture/L04/outline.md
@@ -4,7 +4,9 @@
 
 ### 1) Announcement about Markdown and MATLAB publishing to PDF for first homework.
 
-### 2) Conditional branching
+## To we will talk about [**control flow**](http://www.mathworks.com/help/matlab/control-flow.html) in MATLAB.
+
+### Preliminary
 
 (Chapter 4 in book)
 
@@ -16,9 +18,68 @@ True=1 and False=0
 	b) Logical Operators (&, | , ~) (Section 4.1.2)
 	c) Conditional branching If, elseif, else, end (Section 4.1.3)
 
-### 3) In class exercise
 
-	a) Do order of operations exercises
+### 2) Conditional branching with if-statements
+
+A [**branch**](https://en.wikipedia.org/wiki/Branch_(computer_science)) is an instruction in a computer program that can cause a computer to begin executing a different instruction sequence and thus deviate from its default behavior of executing instructions in order. **Branch** (or branching, branched) may also refer to the act of switching execution to a different instruction sequence as a result of executing a branch instruction. A branch instruction can be either an unconditional branch, which always results in branching, or a conditional branch, which may or may not cause branching, depending on some condition. Branch instructions are used to implement control flow in program loops and conditionals (i.e., executing a particular sequence of instructions only if certain conditions are satisfied).
+
+In computer science, [**conditional statements**](https://en.wikipedia.org/wiki/Conditional_(computer_programming)), **conditional expressions** and **conditional constructs** are features of a programming language, which perform different computations or actions depending on whether a programmer-specified boolean condition evaluates to true or false. Apart from the case of branch predication, this is always achieved by selectively altering the control flow based on some condition.
+
+[Flowchart image 1](https://en.wikipedia.org/wiki/Conditional_(computer_programming)#/media/File:If-Then-Else-diagram.svg)
+
+[Flowchart image 2](https://en.wikipedia.org/wiki/Conditional_(computer_programming)#/media/File:IF-THEN-ELSE-END_flowchart.svg)
+
+	if expression
+    	statements
+	elseif expression
+    	statements
+	else
+    	statements
+	end
+
+### 3) Conditional branching with switch-statements
+
+In computer programming languages, a [**switch**](https://en.wikipedia.org/wiki/Switch_statement) statement is a type of selection control mechanism used to allow the value of a variable or expression to change the control flow of program execution via a multiway branch. In MATLAB it looks like:
+
+	switch switch_expression
+   		case case_expression
+      		statements
+   		case case_expression
+      		statements
+    		...
+   		otherwise
+      		statements
+	end
+
+### 4) For Loops
+
+	"There are a number of other ways of controlling the flow of your code,
+	for example the for command can be used when we want to repeat the same
+	operation a specific number of times. For loops are commonly used in
+	other programming languages such as FORTRAN to repeat specific tasks,
+	they are also possible in MATLAB but computationally expensive, so they
+	should be avoided when possible. For loops depend on a variable which is
+	set as a counter that will run for a specific number of cycles. 
+	
+The syntax looks like:
+	
+	for index = values
+   		statements
+	end
+	
+### 5) While
+
+[**while**](http://www.mathworks.com/help/matlab/ref/while.html) evaluates an expression, and repeats the execution of a group of statements in a loop while the expression is true. An expression is true when its result is nonempty and contains only nonzero elements (logical or real numeric). Otherwise, the expression is false.
+
+	while expression
+   		statements
+	end
+
+### 6) In class exercise
 
 	a) Create an array that starts at 0.0 and ends at the 10.5. Make the array spacing 0.1.
 	b) Using relation testing, make a new vector that contains a 1 if the element in your array is less than 5 and a 0 if your element is greater than or equal to 5.
+	c) Compute pi/4 using: 
+	
+$$\sum_{n=0}^{\infty} \frac{(-1)^n}{2n+1} = \frac{\pi}{4}$$
+
---
+++ Lecture/L05/practical_5.m
@@ -0,0 +1,148 @@
+clear all
+close all
+
+
+
+
+
+
+
+%% Plotting
+%
+% Now that we can make a vector, let's plot a sine function. First we need
+% to build the independent variable _x_. Let's make _x_ go from 0 to pi,
+% with a sample interval of pi/180. 
+dx = pi / 180;
+x = 0 : dx : 2*pi - dx;
+%%
+% Let's compute the dependent variable _y_ = sin(x).
+y = sin(x); % keep in mind that this function need input in radians. If _x_ were in degrees we would use _sind()_
+%%
+% To plot we use MATLAB's built in plot command
+plot( x, y );
+%%
+% We can see straight away that this is a very bare plot. In order to make
+% a better plot we want to add axes labels, remove extra white space, and
+% maybe even add a legend or title. We have a few options to accomplish
+% this. I will talk about 2 of them.
+%%
+% 
+% # Using the graphic's GUI
+% # Directly at the command line
+% 
+%%
+% 1.
+plot( x, y);
+%%
+% 2.
+plot( x, y); xlabel('X'); ylabel('Y'); title('Our first plot');
+axis('tight'); % type "help axis" to see other built-in options
+%%
+% For much more on MATLAB plotting look at 
+% http://www.mathworks.com/help/matlab/plotting-basics.html
+% You can plot surfaces, 3D shapes, etc. In reality you can almost plot
+% anything you want. It can some effort to figure out how, but MATLAB
+% usually has examples at the bottom of every "help" or a quick online
+% search can yield useful information. For example, a surface plot looks
+% like
+surf(peaks)
+
+
+%% Useful things to know about plotting and MATLAB graphics objects
+%
+% There are many more graphic properties that are worth knowing. Here we
+% go through a few of the more important properties for creating nice
+% graphics.
+close all; % First will close all previous graphic windows.
+
+fontName = 'Times'; % set the font name we want to use
+fontWeight = 'Normal'; % set the font weight we want to use
+
+h = figure('Color','White'); % create a new graphic and assign it "h", which is the _figure handle_
+set(h,'PaperUnits','Inches'); % set the paper size units
+set(h, 'Units', 'Inches','Position',[1 1 8 4]); % set the graphic size unit and dimension
+
+plot( x, y, 'Color', 'r', 'LineWidth', 4 ); % change the color and line size
+xlabel('X','FontName',fontName , 'FontWeight',fontWeight ); % set xlabel and font properties
+ylabel('Y','FontName',fontName , 'FontWeight',fontWeight ); 
+title('Our first plot','FontName',fontName , 'FontWeight',fontWeight );
+axis('tight');
+
+%%
+% Notes: 
+% You can use 'rgb' color codes, 'cmyk' or the built-in shortcuts like 'r'
+% You can change the line style
+
+%%
+% Let's say we don't want to set individual properties to all have the same
+% font characteristics. We could use the _findall()_ function to set
+% everything in this plot the same font size 
+set( findall( h, '-property', 'FontSize' ), 'FontSize', 36 );
+%%
+% Let's say we had a more complex function that was y = sin(wt) and we
+% wanted the Y-axis label to reflect this. We could use the built in
+% 'TeX' interpreter or we could use the 'LaTeX' interpreter.
+set( findall( h, '-property', 'Interpreter' ), 'Interpreter', 'Tex' );
+ylabel('Y = sin(\omega X)','FontName',fontName , 'FontWeight',fontWeight ); % this can't handle \sin() though
+%%
+% Use 'LaTeX'
+set( findall( h, '-property', 'Interpreter' ), 'Interpreter', 'Latex' );
+ylabel('$Y = \sin(\omega X)$','FontName',fontName , 'FontWeight',fontWeight ); % NOTE the added $_$ around the label -- just like in LaTeX inline math command 
+%%
+% For more information about setting the text properties look at 
+% http://www.mathworks.com/help/matlab/ref/text-properties.html
+%%
+% Let's save our figure now. You can save a figure manually from the
+% Graphics -> File -> SaveAs to any of the available formats. Or you can
+% open Graphics -> File -> Export Setup to modifies figure properties
+% before exporting to one of the available formats. You can also save a
+% figure from the command line using the _print()_ command.
+print( h, './firstFigure.png', '-dpng' ); % save figure as png. Type "help print" to see all options.
+%%
+% You should notice that the dimensions of the figure in the saved file are
+% not the same as the dimension of the figure that we manually set, nor how
+% it looks on the screen when we plot. To resolve this we need to set one
+% more property.
+set(h, 'PaperPositionMode','Auto'); % this command makes saving the figur
+print( h, './firstFigure2.png', '-dpng' );
+%%
+% MATLAB's built-in ".fig" format is worth knowing about. Type "help
+% savefig". This allows you to save a graphic object and open it later with
+% the capability to edit the graphic properties as we have just done. This
+% is useful for saving publication figures. If you need to change something
+% later, this can be a quick way to modify the figure.
+help savefig
+%%
+%
+savefig(h,'./firstFigure.fig');
+close all
+openfig('./firstFigure.fig');
+
+
+
+
+%% Functions
+
+% In order to improve the readability and reduce writing repetitive lines
+% of code we can write functions to do simple tasks. The built-in MATLAB
+% _plot()_ command is an example of a function. I have written a function
+% that we can then call to do a task. Open the file myMinimum.m
+x = -100:100;
+
+[minVal, minIdx ] = myMinimum( x )
+%%
+% Let's look at the help for this function. Everything in comments
+% immediately following the "function" declaration is part of the preamble
+% of this functiona and will show up in the "help myMinimum" command.
+help myMinimum
+
+%%
+% That is it for Lecture 1!
+
+a = 1 + 1i;
+
+
+%% 
+% Written by _Dylan Mikesell_,
+% with excerpts from
+% http://web.gps.caltech.edu/classes/ge11d/doc/matlab_Resource_Seminar.pdf 
