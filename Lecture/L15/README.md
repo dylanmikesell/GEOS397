68ad9542b461348b52457ce83941752aa8621d46
Dylan Mikesell <dylan.mikesell@gmail.com>
2016-10-12 10:53:45 -0600

    L15 materials.

--- Lecture/L15/README.md
+++ Lecture/L15/README.md
@@ -4,7 +4,19 @@
 
 ### 1) Update from github
 
-### 2) Numerical integration
+### 2) Review HW4 solutions
+
+### 3) Matrix plotting and colorbars
+
+__surf()__
+
+__imagesc()__
+
+__colormap()__
+
+__colorbar()__
+
+### 4) Numerical integration
 
 _Numerical integration_ concerns several different tasks, including numerical evaluation of integrals and the numerical solution of ordinary differential equations. The term __quadrature__ is often used for the numerical evaluation of a definite integral. Modern quadrature algorithms automatically/adaptively vary integration step size.
 
@@ -16,11 +28,11 @@ _Numerical integration_ concerns several different tasks, including numerical ev
 
 4) Extrapolated Simpson's rule
 
-### 3) Examples and _quadgui_
+### 4) Examples and _quadgui_
 
 In these examples we integrate a _function_ over a user specified interval.
 
-### 4) Integration of discrete data (i.e. a vector)
+### 5) Integration of discrete data (i.e. a vector)
 
 What happens if our function is only known at a finite number of points?
 
--- Lecture/L15/practical_15.m
+++ Lecture/L15/practical_15.m
@@ -2,7 +2,75 @@ clear all
 close all
 clc
 
-% Lecture 14 practical: Numerical integration
+% Lecture 15 practical:
+
+%% Colormaps and colorbars
+
+[x, y, F] = peaks(25); % load a generic function from MATLAB
+
+% plot the surface
+figure;
+surf(x,y,F); % surf(x-grid,y-grid, function(x,y))
+
+xlabel('x'); ylabel('y'); zlabel('F(x,y)');
+
+%% or plot the matrix with colorbar and label
+
+x1 = x( 1, : ); % get the unique x-values
+y1 = y( :, 1 ); % get the unique y-values
+
+figure;
+imagesc( x1, y1, F ); % imagesc(x-vector,y-vector, function(x,y))
+xlabel('x'); ylabel('y'); 
+c = colorbar; % make a colorbar and save a handle
+c.Label.String = 'F(x,y)';
+% or
+% ylabel(c,'F(x,y)');
+
+
+%% let's look at the colormap now
+
+% more info at
+% https://www.mathworks.com/help/matlab/ref/colormap.html?searchHighlight=colormap
+
+clc; % clear command window
+
+figure;
+imagesc( x1, y1, F );
+colormap % Each row defines a three-element RGB triplet.
+
+%% change the colormap
+
+figure;
+imagesc( x1, y1, F );
+colormap( jet ) % Each row defines a three-element RGB triplet.
+colorbar;
+
+%% change the colormap interval
+
+figure;
+imagesc( x1, y1, F );
+colormap( jet( 30 ) ) % Each row defines a three-element RGB triplet.
+colorbar;
+
+% try changing the number inside of jet and look at the output
+
+%% what about the axis the limits for the colorbar
+
+figure;
+imagesc( x1, y1, F );
+colormap( jet ); % Each row defines a three-element RGB triplet.
+colorbar;
+caxis( [-10 30] ); % caxis( [cmin cmax] )
+
+
+
+
+
+%%
+%--------------------------------------------------------------------------
+% Numerical integration
+%--------------------------------------------------------------------------
 
 addpath('./ncm'); % tell MATLAB to look in this folder for functions
 
@@ -80,10 +148,18 @@ quadgui( f, 0, 1, tol, z, w); % all values passed after tol will be given to the
 
 % How do we do integration when we have discretely sampled data?
 
-x = linspace( 0, pi, 51 );
+x = linspace( 0, pi, 11 );
 y = sin(x);
 
 plot(x,y,'k*'); xlabel('x [rad]'); ylabel('sin(x)'); axis('tight');
+
+% trapz  Trapezoidal numerical integration.
+Q = trapz( x, y ); % trapz(x-vector, y-vector)
+
+f = @(x) sin(x);
+Qi = integral( f, 0, pi, 'AbsTol', 1.e-8 ); % new method
+
+
 
 
 %%
